ac2ac50923b50589c94da195322a8a4064deb797
Yan Chen <yan.chen@fastly.com>
2025-09-03 15:34:25 -0700

    add io interface into a package, so that we can use the interface later. This fixes the broken wit

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ wasm-tools component embed ../../../wit --world imports virt.wasm -o virt.wasm
 wasm-tools component new virt.wasm -o virt.wasm
 wasm-tools component embed ../../../wit --world exports exports.wasm -o exports.wasm
 wasm-tools component new exports.wasm -o exports.wasm
-wac compose --dep virt:imports=./virt.wasm --dep root:component=./main.wasm --dep virt:exports=./exports.wasm ../../../compose.wac -o composed.wasm
+wac compose --dep wasi:io=../../../wit/deps/io.wit --dep virt:imports=./virt.wasm --dep root:component=./main.wasm --dep virt:exports=./exports.wasm ../../../compose.wac -o composed.wasm
 popd
 
